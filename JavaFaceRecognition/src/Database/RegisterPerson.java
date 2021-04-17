575744836687280875033acf1e9a9001fc006051
Alexandru Perecz <alex_perecz@yahoo.com>
2021-04-17 22:18:52 +0300

    SingUpGUI
    Da

--- JavaFaceRecognition/src/Database/RegisterPerson.java
+++ JavaFaceRecognition/src/Database/RegisterPerson.java
@@ -1,66 +1,159 @@
 package Database;
 
-import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.Font;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.EventQueue;
 
 import javax.swing.JFrame;
-import javax.swing.JLabel;
+import java.awt.Color;
 import javax.swing.JPanel;
+import java.awt.BorderLayout;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import java.awt.SystemColor;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.Button;
 import javax.swing.JTextField;
+import javax.swing.JSeparator;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.JCheckBox;
+import javax.swing.SwingConstants;
+import javax.swing.BoxLayout;
+import java.awt.FlowLayout;
 
-public class RegisterPerson extends JFrame implements ActionListener {
+public class RegisterPerson extends JFrame{
 
-	JLabel textLabel ;
-	JPanel frameColor ;
-	JTextField usernameField;
-	JTextField passwordField;
+	private JFrame frame;
+	private JTextField usernameField;
+	private JTextField mailField;
+	private JTextField passwordField;
+	private JTextField repeatPasswordField;
+	private ImageIcon image;
 	
-	RegisterPerson(){
-		
-		textLabel = new JLabel();
-		frameColor = new JPanel();
-		usernameField = new JTextField();
-		passwordField = new JTextField();
-
-	textLabel.setText("This person gets a ID x");
-	textLabel.setVerticalTextPosition(JLabel.NORTH);
-	textLabel.setHorizontalAlignment(JLabel.CENTER);
-	textLabel.setBounds(150, 50, 500, 100);
-	textLabel.setBackground(Color.GRAY);
-	textLabel.setOpaque(true);
-	textLabel.setFont(new Font("Times New Roman",Font.PLAIN, 22));
-
-
-	frameColor.setBackground(Color.DARK_GRAY);
-	frameColor.setSize(new Dimension(800,800));
-	
-	usernameField.setPreferredSize(new Dimension(250,40));
-	
-	passwordField.setPreferredSize(new Dimension(250,40));
-	
-	this.setTitle("Login");
-	this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	this.setResizable(false);
-	this.setSize(800,800);
-	this.setLayout(null);	
-	
-	frameColor.add(passwordField);
-	frameColor.add(usernameField);
-	this.add(textLabel);
-	this.add(frameColor);
-	
-	this.setVisible(true);
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					RegisterPerson window = new RegisterPerson();
+					window.setUndecorated(isDefaultLookAndFeelDecorated());
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
 	}
- public static void main (String[] args){
-	 new RegisterPerson();
- }
-@Override
-public void actionPerformed(ActionEvent e) {
-	// TODO Auto-generated method stub
-	
-}
 
+	/**
+	 * Create the application.
+	 */
+	public RegisterPerson() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBackground(Color.WHITE);
+		frame.getContentPane().setBackground(Color.WHITE);
+		frame.getContentPane().setLayout(null);
+		
+		JPanel leftPanel = new JPanel();
+		leftPanel.setBackground(Color.DARK_GRAY);
+		leftPanel.setBounds(0, 0, 354, 461);
+		frame.getContentPane().add(leftPanel);
+		leftPanel.setLayout(null);
+		
+		
+		JLabel imageLabel = new JLabel("");
+		imageLabel.setBounds(-52, 0, 443, 320);
+		leftPanel.add(imageLabel);
+		imageLabel.setIcon(new ImageIcon("C:\\Users\\Alex\\OneDrive\\Desktop\\leftBgd.jpg"));
+		
+		JLabel leftText = new JLabel("FACE RECOGNITION R&A");
+		leftText.setForeground(Color.WHITE);
+		leftText.setFont(new Font("Microsoft YaHei", Font.BOLD, 18));
+		leftText.setBounds(63, 323, 231, 64);
+		leftPanel.add(leftText);
+		
+		Button signUp = new Button("SignUp");
+		signUp.setForeground(SystemColor.textHighlightText);
+		signUp.setBackground(new Color(25, 130, 183));
+		signUp.setBounds(382, 344, 324, 35);
+		signUp.setFont(new Font("Arial", Font.BOLD, 12));
+		frame.getContentPane().add(signUp);
+		
+		usernameField = new JTextField();
+		usernameField.setBounds(364, 43, 354, 35);
+		frame.getContentPane().add(usernameField);
+		usernameField.setColumns(10);
+		
+		JSeparator separatorUsername = new JSeparator();
+		separatorUsername.setBounds(364, 77, 354, 2);
+		frame.getContentPane().add(separatorUsername);
+		
+		JLabel usernameLabel = new JLabel("USERNAME");
+		usernameLabel.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		usernameLabel.setBounds(364, 21, 76, 23);
+		frame.getContentPane().add(usernameLabel);
+		
+		mailField = new JTextField();
+		mailField.setColumns(10);
+		mailField.setBounds(364, 111, 354, 35);
+		frame.getContentPane().add(mailField);
+		
+		JSeparator separatorMail = new JSeparator();
+		separatorMail.setBounds(364, 145, 354, 2);
+		frame.getContentPane().add(separatorMail);
+		
+		JLabel mailLabel = new JLabel("EMAIL");
+		mailLabel.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		mailLabel.setBounds(364, 89, 76, 23);
+		frame.getContentPane().add(mailLabel);
+		
+		passwordField = new JTextField();
+		passwordField.setColumns(10);
+		passwordField.setBounds(364, 179, 354, 35);
+		frame.getContentPane().add(passwordField);
+		
+		JSeparator separatorPassword = new JSeparator();
+		separatorPassword.setBounds(364, 213, 354, 2);
+		frame.getContentPane().add(separatorPassword);
+		
+		JLabel passwordLabel = new JLabel("PASSWORD");
+		passwordLabel.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		passwordLabel.setBounds(364, 157, 76, 23);
+		frame.getContentPane().add(passwordLabel);
+		
+		repeatPasswordField = new JTextField();
+		repeatPasswordField.setColumns(10);
+		repeatPasswordField.setBounds(364, 247, 354, 35);
+		frame.getContentPane().add(repeatPasswordField);
+		
+		JSeparator separatorRepeatpw = new JSeparator();
+		separatorRepeatpw.setBounds(364, 281, 354, 2);
+		frame.getContentPane().add(separatorRepeatpw);
+		
+		JLabel repeatpwLabel = new JLabel("REPEAT PASSWORD");
+		repeatpwLabel.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		repeatpwLabel.setBounds(364, 225, 117, 23);
+		frame.getContentPane().add(repeatpwLabel);
+		
+		JCheckBox termsAndConditions = new JCheckBox("I have read and agree / agreed with the terms and conditions");
+		termsAndConditions.setFont(new Font("Tahoma", Font.ITALIC, 11));
+		termsAndConditions.setBackground(SystemColor.activeCaption);
+		termsAndConditions.setForeground(Color.WHITE);
+		termsAndConditions.setBounds(365, 300, 324, 23);
+		frame.getContentPane().add(termsAndConditions);
+		frame.setBounds(100, 100, 750, 430);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setResizable(false);
+		
+	}
 }
--- JavaFaceRecognition/src/main/Main.java
+++ JavaFaceRecognition/src/main/Main.java
@@ -1,10 +1,13 @@
 package main;
 
+import Database.RegisterPerson;
+
 public class Main {
 
 	public static void main(String[] args) {
-			new MyFrame();
+			//new MyFrame();
 			//System.out.println("Hello abc");
+		new RegisterPerson();
 	}
 
 }
