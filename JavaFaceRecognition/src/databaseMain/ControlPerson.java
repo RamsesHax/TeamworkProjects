6207b0c8d96e13b9a46cc463044da4de1ea77d7c
Alexandru Perecz <alex_perecz@yahoo.com>
2021-04-20 18:36:14 +0300

    Register req + warning frame

--- JavaFaceRecognition/src/databaseMain/ControlPerson.java
+++ JavaFaceRecognition/src/databaseMain/ControlPerson.java
@@ -3,14 +3,24 @@ package databaseMain;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
 public class ControlPerson {
 	ConDatabase connection = new ConDatabase();
 	
+	public class WarningFrame {  
+		JFrame warning;  
+		WarningFrame(){  
+		    warning = new JFrame();  
+		    JOptionPane.showMessageDialog(warning,"All the fields have to be valid","Warning",JOptionPane.WARNING_MESSAGE);     
+		}
+	}
+	
 	public void insert(ModelPerson person) {
 		try {
-			//in if sa intre si lucruri precum nr de caractere si eventual un format.
-			if(!person.getUsername().equals("") || ...) {
-			
+			if(!person.getUsername().isBlank() || !person.getEmail().isBlank() || !person.getDateOfBirth().isBlank() || !person.getAddress().isBlank() || person.getEmail().contains("@gmail") || person.getEmail().contains("@email") ) {
+			//isBlank metoda verifica daca este empty sau contine doar spatii
 			connection.connect();
 			PreparedStatement pst = connection.connection.prepareStatement("INSERT INTO accounts (id, user, email , date, address) values (?, ?, ?, ?, ?)");
 			pst.setInt(1, person.getId());
@@ -23,10 +33,20 @@ public class ControlPerson {
 			connection.disconnect();
 			
 			}else {
-				//instantiate new JOptionPane cu mesajul like :"Toate campurile trebuie sa fie valide"
+				new WarningFrame();
 			}
 		}catch(SQLException e) {
 			System.out.println("E :"+e);
 		}
 	}
+	
+	
+	//Verificare daca adresa de mail exista(daca nu manca multe resurse o implementare de genu, am s-o fac)
+/*	public boolean isValidEmailAddress(String email) {
+        String ePattern = "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\])|(([a-zA-Z\\-0-9]+\\.)+[a-zA-Z]{2,}))$";
+        java.util.regex.Pattern p = java.util.regex.Pattern.compile(ePattern);
+        java.util.regex.Matcher m = p.matcher(email);
+        return m.matches();
+ } */
+
 }
