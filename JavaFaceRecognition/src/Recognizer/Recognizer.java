a34c30e1a28da8939a2dcc23d97ac17a171946ac
RamsesHax <onetiu.rares@yahoo.com>
2021-04-28 01:52:34 +0300

    GUI done

--- JavaFaceRecognition/src/Recognizer/Recognizer.java
+++ JavaFaceRecognition/src/Recognizer/Recognizer.java
@@ -18,22 +18,27 @@ public class Recognizer extends JFrame {
 	 */
 	private static final long serialVersionUID = 1L;
 	
-	private JFrame frame = new JFrame();
-	JPanel bkgPanel = new JPanel();
-	JLabel bkgImage = new JLabel();
-	ImageIcon bkg ;
+	private JFrame frame;
+	JPanel bkgPanel;
+	JLabel bkgLabel;
+	ImageIcon bkgImage ;
+	JLabel cameraLabel;
 	
 	public Recognizer() throws IOException {
+		frame = new JFrame();
 		getFrame().setLayout(null);
 		getFrame().setBounds(250,250, 800, 480);
 		
-		bkgPanel.setBounds(0,0,800,450);
+		bkgPanel = new ImagePanel();
+		bkgPanel.setBounds(0,0,800,480);
+		bkgPanel.setLayout(null);
 		
-		bkgImage.setBounds(0,0,800,450);
-		bkg = new ImageIcon();
-		bkg.setImage(ImageIO.read(getClass().getResource("/img/recog.jpg")));
-		bkgImage.setIcon(bkg);
+		cameraLabel = new JLabel("");
+		cameraLabel.setBounds(262,54,276,342);
+		cameraLabel.setBackground(Color.red);
+		cameraLabel.setOpaque(true);
 		
+		bkgPanel.add(cameraLabel);
 		getFrame().add(bkgPanel);
 		getFrame().setVisible(true);
 	}
