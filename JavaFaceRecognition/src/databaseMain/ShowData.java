b7f931715ef116360609b96654e33c702403dc21
Alexandru Perecz <alex_perecz@yahoo.com>
2021-05-05 16:31:50 +0300

    Update ShowData.java

--- JavaFaceRecognition/src/databaseMain/ShowData.java
+++ JavaFaceRecognition/src/databaseMain/ShowData.java
@@ -23,47 +23,52 @@ public class ShowData extends JFrame {
 	
 	
 	public ShowData() {
-		 try {
-		      File myObj = new File("D:\\filename.txt");
+		 
+		
+		setFrame(new JFrame());
+		getFrame().setBounds(100, 100, 698, 430);
+		getFrame().setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		getFrame().setResizable(false);
+		getFrame().setLayout(new BorderLayout());
+		
+		panel = new JPanel();
+		panel.setBounds(0, 0, 698, 460);
+		panel.setBackground(Color.red);
+		panel.setLayout(new BorderLayout());
+		
+		textArea = new JTextArea();
+		textArea.setFont(new Font("Serif", Font.ITALIC, 16));
+		textArea.setLineWrap(true);
+		textArea.setWrapStyleWord(true);
+		textArea.setEditable(false);
+		textArea.setBackground(Color.RED);
+		
+		try {
+		      File myObj = new File("D:\\filename.txt"); // aici trebuie bagata baza de date
 		      Scanner myReader = new Scanner(myObj);
 		      while (myReader.hasNextLine()) {
 		        String data = myReader.nextLine();
-		        textArea.setText(data);
+		    	textArea.setText(data);
+		    	textArea.setOpaque(false);
 		      }
 		      myReader.close();
 		    } catch (FileNotFoundException e) {
 		      System.out.println("An error occurred.");
 		      e.printStackTrace();
 		    }
+		textArea.setForeground(Color.WHITE);
 		
-		setFrame(new JFrame());
-		getFrame().setBounds(100, 100, 698, 430);
-		getFrame().setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		getFrame().setResizable(false);
-		getFrame().setLayout(null);
-		
-		panel = new JPanel();
-		panel.setBounds(0, 0, 698, 460);
-		panel.setBackground(Color.red);
-		panel.setLayout(null);
-		
-		textArea = new JTextArea();
-		textArea.setFont(new Font("Serif", Font.ITALIC, 16));
-		textArea.setLocation(0,0);
-		textArea.setLineWrap(true); // taie din text si-l pune sub(un fel de resize al textului pentru a fii incadrat in frame)
-		textArea.setWrapStyleWord(true);
-		textArea.setEditable(false);
-		//textArea.setText("Ana are mereasAna are mereasAna are mereasAna are mereasAna are mereasAna are mereasAna are mereasAna are mereasAna are mereasAna are mereasAna are mereasAna are mereasAna are mereasAna are mereasAna are mereas");
-		// nu apare nici un fel de text pe pane
-		textArea.setBackground(Color.white);
-		
-		scrollBar = new JScrollPane(textArea, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+		scrollBar = new JScrollPane(textArea);
 		scrollBar.setSize(new Dimension(20, 390));
 		scrollBar.setLocation(660,0);
 		
-		panel.add(textArea);
+		scrollBar.getViewport().setOpaque(false);
+		scrollBar.setOpaque(false);
+		
 		panel.add(scrollBar);
 		getFrame().add(panel);
+	
+		
 			
 		
 		getFrame().setVisible(true);
